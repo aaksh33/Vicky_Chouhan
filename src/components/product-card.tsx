7078cd99e5a27cc031e3848ffb98805f6f91e6ac
Sonu Rai <sonurai7011@gmail.com>
2025-10-13 22:57:01 +0530

    fix

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -23,8 +23,8 @@ type Product = {
 
 type ProductCardProps = {
   product: Product;
-  onAddToCart: (e: React.MouseEvent, product: any) => void;
-  onBuyNow: (e: React.MouseEvent, product: any) => void;
+  onAddToCart?: (e: React.MouseEvent, product: any) => void;
+  onBuyNow?: (e: React.MouseEvent, product: any) => void;
 };
 
 export default function ProductCard({ product, onAddToCart, onBuyNow }: ProductCardProps) {
@@ -165,22 +165,24 @@ export default function ProductCard({ product, onAddToCart, onBuyNow }: ProductC
             </div>
           </div>
           
-          <div className="flex gap-2">
-            <button
-              onClick={(e) => onAddToCart(e, product)}
-              disabled={currentQty === 0}
-              className="flex-1 bg-yellow-400 hover:bg-yellow-500  text-gray-900 font-bold py-2 text-xs sm:text-sm transition-all rounded-sm disabled:opacity-40 disabled:cursor-not-allowed shadow-md"
-            >
-              ADD TO CART
-            </button>
-            <button
-              onClick={(e) => onBuyNow(e, product)}
-              disabled={currentQty === 0}
-              className="flex-1 bg-orange-500 hover:bg-orange-600 text-white hidden sm:block font-bold py-2 text-sm transition-all rounded-sm disabled:opacity-40 disabled:cursor-not-allowed shadow-md"
-            >
-              BUY NOW
-            </button>
-          </div>
+          {onAddToCart && onBuyNow && (
+            <div className="flex gap-2">
+              <button
+                onClick={(e) => onAddToCart(e, product)}
+                disabled={currentQty === 0}
+                className="flex-1 bg-yellow-400 hover:bg-yellow-500  text-gray-900 font-bold py-2 text-xs sm:text-sm transition-all rounded-sm disabled:opacity-40 disabled:cursor-not-allowed shadow-md"
+              >
+                ADD TO CART
+              </button>
+              <button
+                onClick={(e) => onBuyNow(e, product)}
+                disabled={currentQty === 0}
+                className="flex-1 bg-orange-500 hover:bg-orange-600 text-white hidden sm:block font-bold py-2 text-sm transition-all rounded-sm disabled:opacity-40 disabled:cursor-not-allowed shadow-md"
+              >
+                BUY NOW
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
